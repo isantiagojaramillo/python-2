66b0412f19a1fc93eb82d96d69f36140e52499b6
isantiagojaramillo <sajaramillogr@cesde.net>
2024-02-06 18:09:42 -0500

    Python: Strings

---
+++ 02-strings.py
@@ -0,0 +1,13 @@
+# STRING METHODS
+
+text = "Hello Santi";
+
+print(text.upper());
+print(text.lower);
+print(text.find("S"));
+print(text.find("Santi"));
+print(text.replace("San", "Happy Chanchito"));
+newText = text.replace("San", "Happy Chanchito");
+print(text, newText);
+print("Hello" in text);
+
