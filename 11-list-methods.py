677f45637a9b335f771b594421c07495718d1040
isantiagojaramillo <sajaramillogr@cesde.net>
2024-02-08 15:24:20 -0500

    Python: list mehods

---
+++ 11-list-methods.py
@@ -0,0 +1,9 @@
+languages = ["Python", "Javascript", "PHP", "Java"];
+languages.insert(3, "Go");
+languages.insert(0, "C");
+languages.remove("Java");
+print("PHP" in languages);
+# languages.clear();
+
+
+print(len(languages));
