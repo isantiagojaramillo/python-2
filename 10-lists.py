7b34f0020cc7581594b6df8de9165d6ff7adc775
isantiagojaramillo <sajaramillogr@cesde.net>
2024-02-08 15:11:10 -0500

    Python: lists

---
+++ 10-lists.py
@@ -0,0 +1,9 @@
+languages = ["Python", "Javascript", "PHP", "Java"];
+print(languages);
+# print(languages[1]);
+languages[3] = "Go";
+# print(languages);
+# print(languages[-1]);
+print(languages[1:3]);
+print(languages[:3]);
+print(languages[1:]);
