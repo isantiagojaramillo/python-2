f215f165eafd0d91f6ef90c0b09fc256686c7111
isantiagojaramillo <sajaramillogr@cesde.net>
2024-02-08 14:28:05 -0500

    Python: convert

---
+++ 06-convert.py
@@ -0,0 +1,11 @@
+result = input("Enter your age: ");
+
+print(type(result));
+
+# int function will take the variable and will convert it into a numeric variable
+number = int(result);
+print(number + 2);
+
+str(22);
+float("22.13");
+bool("");
