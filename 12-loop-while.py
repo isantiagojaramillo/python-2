f644179c26b221b914dc2a5092ef89ae1bc09cd3
isantiagojaramillo <sajaramillogr@cesde.net>
2024-02-08 15:33:45 -0500

    Python: while loop

---
+++ 12-loop-while.py
@@ -0,0 +1,19 @@
+languages = ["Python", "Javascript", "PHP", "Java"];
+
+# i = 1;
+
+# while i <= 5:
+#     print(i);
+#     i = i + 1;
+
+# i = 1;
+
+# while i <= 5:
+#     print(i * "Chanchito");
+#     i = i + 1;
+
+i = 0;
+
+while i < len(languages):
+    print(languages[i]);
+    i = i + 1;
