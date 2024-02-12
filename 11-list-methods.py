d628e31ad16e824bca2f66e5acd17b412f25c5fe
isantiagojaramillo <sajaramillogr@cesde.net>
2024-02-12 11:02:15 -0500

    Python: new list methods

--- 11-list-methods.py
+++ 11-list-methods.py
@@ -2,8 +2,38 @@ languages = ["Python", "Javascript", "PHP", "Java"];
 languages.insert(3, "Go");
 languages.insert(0, "C");
 languages.remove("Java");
+languages.append("ReactJS");
+print(languages);
+
 print("PHP" in languages);
 # languages.clear();
 
 
 print(len(languages));
+
+tasks = ['todo 1', 'todo 2', 'todo 3'];
+new_list = languages + tasks;
+print(new_list);
+
+index = new_list.index('todo 2');
+new_list[index] = 'todo changed';
+print(new_list);
+
+new_list.pop();
+print(new_list);
+
+new_list.pop(0);
+print(new_list);
+
+new_list.reverse();
+print(new_list);
+
+numbers = [1, 4, 6 , 3];
+numbers.sort();
+print(numbers);
+
+strings = ['re', 'ab', 'ed'];
+strings.sort();
+print(strings);
+
+new_list.sort();
