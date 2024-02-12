55b07ebfff850a42a610744e9c228d5596c4e1dd
isantiagojaramillo <sajaramillogr@cesde.net>
2024-02-12 10:09:56 -0500

    Python: Strings, new functions

--- 02-strings.py
+++ 02-strings.py
@@ -11,3 +11,17 @@ newText = text.replace("San", "Happy Chanchito");
 print(text, newText);
 print("Hello" in text);
 
+print(text.count('a'))
+
+print(text.swapcase())
+print(text.startswith('She'))
+print(text.endswith('Rust'))
+print(text.replace('Santi', 'Python'))
+
+text_2 = 'este es un titulo'
+print(text_2)
+print(text_2.capitalize())
+print(text_2.title())
+print(text_2.isdigit())
+print("398".isdigit())
+
