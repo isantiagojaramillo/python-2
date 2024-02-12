d7a9c354644dd60c0a4b5ab49fed65588a79b58f
isantiagojaramillo <sajaramillogr@cesde.net>
2024-02-12 12:21:19 -0500

    Python: loop while, updated

--- 12-loop-while.py
+++ 12-loop-while.py
@@ -12,8 +12,25 @@ languages = ["Python", "Javascript", "PHP", "Java"];
 #     print(i * "Chanchito");
 #     i = i + 1;
 
-i = 0;
+# i = 0;
 
-while i < len(languages):
-    print(languages[i]);
-    i = i + 1;
+# while i < len(languages):
+#     print(languages[i]);
+#     i = i + 1;
+
+# counter = 0
+
+# while counter < 20:
+#   counter += 1
+#   if counter == 15:
+#     break
+#   print(counter)
+
+
+counter = 0
+
+while counter < 20:
+  counter += 1
+  if counter < 15:
+    continue
+  print(counter)
