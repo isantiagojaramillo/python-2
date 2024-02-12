605a227416bf6ac57f44b9f7bb878f70e1e358c1
isantiagojaramillo <sajaramillogr@cesde.net>
2024-02-12 10:33:59 -0500

    Python: Strings, new functions

--- 02-strings.py
+++ 02-strings.py
@@ -11,17 +11,33 @@ newText = text.replace("San", "Happy Chanchito");
 print(text, newText);
 print("Hello" in text);
 
-print(text.count('a'))
+print(text.count('a'));
 
-print(text.swapcase())
-print(text.startswith('She'))
-print(text.endswith('Rust'))
-print(text.replace('Santi', 'Python'))
+print(text.swapcase());
+print(text.startswith('She'));
+print(text.endswith('Rust'));
+print(text.replace('Santi', 'Python'));
 
-text_2 = 'este es un titulo'
-print(text_2)
-print(text_2.capitalize())
-print(text_2.title())
-print(text_2.isdigit())
-print("398".isdigit())
+print(text[0:5]);
+print(text[10:16]);
+print(text[:10]);
+print(text[5:-1]);
+print(text[5:]);
+print(text[:]);
+print(text[10:16:1]);
+print(text[10:16:2]);
+print(text[::2]);
+
+text_2 = 'este es un titulo';
+print(text_2);
+print(text_2.capitalize());
+print(text_2.title());
+print(text_2.isdigit());
+print("398".isdigit());
+
+size = len(text_2);
+print("Size: "+size);
+
+print(text_2[size -1]);
+print(text_2[-1]);
 
