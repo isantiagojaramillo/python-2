16a1c8b0b4db18f61ab8e1cfa9e426f2f25103ac
isantiagojaramillo <sajaramillogr@cesde.net>
2024-02-12 12:52:11 -0500

    Python: Loop for, updated

--- 13-loop-for.py
+++ 13-loop-for.py
@@ -1,4 +1,45 @@
-languages = ["Python", "Javascript", "PHP", "Java"];
+# languages = ["Python", "Javascript", "PHP", "Java"];
 
-for language in languages:
-    print(language);
+# for language in languages:
+#     print(language);
+
+# for element in range(20):
+#     print(element);
+
+list = [23, 45, 67, 89, 43];
+for i in list:
+    print(i);
+
+tuple = ('Nico', 'Juli', 'Santi');
+for i in tuple:
+    print(i);
+
+dictionary = {
+    'name': 'T-Shirt', 
+    'price': 100,
+    'stock': 89
+}
+
+for key in dictionary:
+    print(key , ':' , dictionary[key]);
+
+for key, value  in dictionary.items():
+    print(key , ':' , dictionary[key]);
+
+people = [
+  {
+    'name': 'nico',
+    'age': 34
+  },
+  {
+    'name': 'zule',
+    'age': 45
+  },
+  {
+    'name': 'santi',
+    'age': 4
+  }
+]
+
+for person in people:
+  print('name =>', person['name'])
